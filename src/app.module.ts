e50914916994d667e335f0e23ab91e6f544a60c5
Mario Mora Núñez <mariomora.ingeniero@gmail.com>
2025-05-08 16:30:00 -0400

    Add Mongoose configuration for production

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config'; // Importa ConfigModule
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { GamesModule } from './games/games.module';
 import { AuthModule } from './auth/auth.module';
@@ -11,7 +11,11 @@ import { AppService } from './app.service';
     ConfigModule.forRoot({
       isGlobal: true
     }),
-    MongooseModule.forRoot(process.env.MONGODB_URI),
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
+      serverSelectionTimeoutMS: 5000, // Tiempo máximo para seleccionar un servidor
+      heartbeatFrequencyMS: 1000, // Frecuencia de chequeo de conexión
+      autoIndex: false, // Desactiva índices automáticos para mejorar rendimiento
+    }),
     GamesModule,
     AuthModule
   ],
